aa2d2b288889a1a43c29fb53526c3654ac22739a
Ketul Parmar <ketul4947@gmail.com>
2025-01-07 23:21:54 +0530

    ongoing

--- Proj_internship/app1/models.py
+++ Proj_internship/app1/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Info(models.Model):
+    objects = None
     Name = models.CharField(max_length=50)
     Email = models.EmailField()
     Password = models.CharField(max_length=8)
-    objects = None
+
+    def __str__(self):
+        return f"{self.Name} - {self.Email} - {self.Password}"
+
+
--- Proj_internship/app1/views.py
+++ Proj_internship/app1/views.py
@@ -1,10 +1,13 @@
+from django.contrib.auth.hashers import check_password
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
-
 from .models import *
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    data = Info.objects.all()
+    print(data)
+    return render(request, 'home.html', {'data': data})
 
 @csrf_protect
 def register(request):
@@ -13,14 +16,17 @@ def register(request):
         Email = request.POST['email']
         Password = request.POST['password']
         Re_password = request.POST['re-password']
-        if Password == Re_password:
-            data = Info(Name=Name,Email=Email,Password=Password)
-            data.save()
-            Msg = "Success"
-            return render(request, 'home.html', context={'MSSG': Msg})
+        if Info.objects.get(Email=Email) == Email:
+            e = "Email already exists"
+            return render(request, 'Register.html', {'e':e})
         else:
-            err = "Password does not match!!"
-            return render(request, 'Register.html', context={'err': err})
+            if Password == Re_password:
+                data = Info(Name=Name,Email=Email,Password=Password)
+                data.save()
+                return redirect(home)
+            else:
+                err = "Password does not match!!"
+                return render(request, 'Register.html', context={'err': err})
     return render(request, 'Register.html')
 
 @csrf_protect
@@ -28,14 +34,17 @@ def login(request):
     if request.method == 'POST':
         Email = request.POST['email']
         Password = request.POST['pass']
-        if Email == Info.objects.get('email'):
-            if Password == Info.objects.get('Password'):
-                return redirect(home)
-            else:
-                err = "Password Incorrect!!"
-                return render(request, 'Login.html', context={'Err': err})
+        try:
+            user = Info.objects.get(Email=Email)  # Use Info model's 'Email' field
+        except Info.DoesNotExist:
+            err = "Email does not exist!"
+            return render(request, 'Login.html', context={'err': err})
+
+            # Check if the password matches using the check_password method (for hashed passwords)
+        if check_password(Password, user.Password):
+            return redirect('home')  # Redirect to home page after successful login
         else:
-            err = "Email Does not exists!!"
-            return render(request, 'Login.html', context={'Err': err})
+            err = "Password is incorrect!"
+            return render(request, 'Login.html', context={'err': err})
 
     return render(request, 'Login.html')
